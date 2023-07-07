581cc7ee3b4a657d451c4a4a7d54456ed430ac06
Andy Chen <chenyangpoppin@hotmail.com>
2023-07-07 10:32:07 +0800

    feat: implement useDebugValue localStorage

--- use-debug/src/useLocalStorage.js
+++ use-debug/src/useLocalStorage.js
@@ -1,6 +1,6 @@
 /**
  * useDebugValue Hook
- * 
+ *
  * things to know:
  * 1. useDebugValue only works in a custom hook
  * 2. if we use multiple useDebugValues, the components data will be transformed to array displaying
@@ -14,14 +14,24 @@ const getValueSlowly = (value) => {
 };
 
 export const useLocalStorage = (key, defaultValue) => {
-  const [value, setValue] = useState(() => {});
+  const [value, setValue] = useState(() => {
+    const jsonValue = window.localStorage.getItem(key);
+    if (jsonValue != null) {
+      return JSON.parse(jsonValue);
+    }
+    if (typeof defaultValue === "function") {
+      return defaultValue();
+    } else {
+      return defaultValue;
+    }
+  });
 
   // useDebugValue([key, value]);
 
   // if we use multiple useDebugValues, the components data will be transformed to array.
   // the data would be something like: [key, value]
-  // useDebugValue(key);
-  // useDebugValue(value);
+  useDebugValue(key);
+  useDebugValue(value);
 
   // ["Hi", {key: "firstName", value: "sss"}]
   // useDebugValue("Hi");
@@ -32,11 +42,18 @@ export const useLocalStorage = (key, defaultValue) => {
   // [function version] only run this code, when we are working with developer tools
   // it will not be running in your production env
   // dev error: Timed out while inspecting element 3.
-  useDebugValue(value, v => getValueSlowly(v))
+  // useDebugValue(value, v => getValueSlowly(v))
 
-  useEffect(() => {}, [key, value, localStorage]);
+  useEffect(() => {
+    if (typeof value === "undefined") {
+      return window.localStorage.removeItem(key);
+    }
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value, window.localStorage]);
 
-  const remove = useCallback(() => {}, []);
+  const remove = useCallback(() => {
+    setValue(undefined)
+  }, []);
 
   return [value, setValue, remove];
 };
