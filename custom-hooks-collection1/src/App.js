b0ce043219b386665b5878fda617d7b42fde3b34
Andy Chen <chenyangpoppin@hotmail.com>
2023-07-11 18:19:59 +0800

    feat: custom hook - useArray

--- custom-hooks-collection1/src/App.js
+++ custom-hooks-collection1/src/App.js
@@ -3,13 +3,15 @@ import ToggleComponent from "./useToggle/ToggleComponent";
 import TimeoutComponent from "./useTimeout/TimeoutComponent";
 import DebounceComponent from "./useDebounce/DebounceComponent";
 import UpdateEffectComponent from "./useUpdateEffect/UpdateEffectComponent";
+import ArrayComponent from "./useArray/ArrayComponent";
 import "./App.css";
 
 function App() {
   // return <ToggleComponent />;
   // return <TimeoutComponent />;
   // return <DebounceComponent />
-  return <UpdateEffectComponent />
+  // return <UpdateEffectComponent />
+  return <ArrayComponent />
 }
 
 export default App;
