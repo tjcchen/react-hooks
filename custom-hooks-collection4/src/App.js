2a5c41fab7a182343d65258a6154a4ee8995f1f9
Andy Chen <chenyangpoppin@hotmail.com>
2023-07-16 17:52:54 +0800

    feat: custom hook - useStateWithValidation

--- custom-hooks-collection4/src/App.js
+++ custom-hooks-collection4/src/App.js
@@ -1,10 +1,12 @@
 // import MediaQueryComponent from "./useMediaQuery/MediaQueryComponent";
-import GeolocationComponent from "./useGeolocation/GeolocationComponent";
+// import GeolocationComponent from "./useGeolocation/GeolocationComponent";
+import StateWithValidationComponent from "./useStateWithValidation/StateWithValidationComponent";
 import "./App.css";
 
 function App() {
   // return <MediaQueryComponent />;
-  return <GeolocationComponent />;
+  // return <GeolocationComponent />;
+  return <StateWithValidationComponent />;
 }
 
 export default App;
---
+++ custom-hooks-collection4/src/useStateWithValidation/StateWithValidationComponent.js
@@ -0,0 +1,24 @@
+/**
+ * Useful for working with forms
+ */
+import useStateWithValidation from "./useStateWithValidation";
+
+const StateWithValidationComponent = () => {
+  const [username, setUsername, isValid] = useStateWithValidation(
+    (name) => name.length > 5,
+    ""
+  );
+
+  return (
+    <>
+      <div>Valid: {isValid.toString()}</div>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+    </>
+  );
+};
+
+export default StateWithValidationComponent;
