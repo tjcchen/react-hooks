ccc0e7aa0b71ce8051d63a9b0ffe11949209ea24
Andy Chen <chenyangpoppin@hotmail.com>
2023-07-17 10:54:09 +0800

    feat: custom hook - useEffectOnce

--- custom-hooks-collection4/src/App.js
+++ custom-hooks-collection4/src/App.js
@@ -1,14 +1,16 @@
 // import MediaQueryComponent from "./useMediaQuery/MediaQueryComponent";
 // import GeolocationComponent from "./useGeolocation/GeolocationComponent";
 // import StateWithValidationComponent from "./useStateWithValidation/StateWithValidationComponent";
-import SizeComponent from "./useSize/SizeComponent";
+// import SizeComponent from "./useSize/SizeComponent";
+import EffectOnceComponent from "./useEffectOnce/EffectOnceComponent";
 import "./App.css";
 
 function App() {
   // return <MediaQueryComponent />;
   // return <GeolocationComponent />;
   // return <StateWithValidationComponent />;
-  return <SizeComponent />;
+  // return <SizeComponent />;
+  return <EffectOnceComponent />;
 }
 
 export default App;
---
+++ custom-hooks-collection4/src/useEffectOnce/EffectOnceComponent.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import useEffectOnce from "./useEffectOnce";
+
+const EffectOnceComponent = () => {
+  const [count, setCount] = useState(0);
+
+  useEffectOnce(() => alert("Hi"));
+
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+    </>
+  );
+};
+
+export default EffectOnceComponent;
