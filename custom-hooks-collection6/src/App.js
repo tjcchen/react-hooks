3cec57b98dca8fcf6f8b6485ee62c26c90987aa0
Andy Chen <chenyangpoppin@hotmail.com>
2023-07-19 14:42:23 +0800

    feat: custom hook - useDebugInformation

--- custom-hooks-collection6/src/App.js
+++ custom-hooks-collection6/src/App.js
@@ -1,10 +1,12 @@
-import OnlineStatusComponent from "./useOnlineStatus/OnlineStatusComponent";
-import RenderCountComponent from "./useRenderCount/RenderCountComponent";
+// import OnlineStatusComponent from "./useOnlineStatus/OnlineStatusComponent";
+// import RenderCountComponent from "./useRenderCount/RenderCountComponent";
+import DebugInformationComponent from "./useDebugInformation/DebugInformationComponent";
 import "./App.css";
 
 function App() {
   // return <OnlineStatusComponent />;
-  return <RenderCountComponent />
+  // return <RenderCountComponent />;
+  return <DebugInformationComponent />;
 }
 
 export default App;
---
+++ custom-hooks-collection6/src/useDebugInformation/DebugInformationComponent.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import useDebugInformation from "./useDebugInformation";
+import useToggle from "../useToggle/useToggle";
+
+const DebugInformationComponent = () => {
+  const [boolean, toggle] = useToggle(false);
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <ChildComponent boolean={boolean} count={count} />
+      <button onClick={toggle}>Toggle</button>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Increment
+      </button>
+    </>
+  );
+};
+
+function ChildComponent(props) {
+  const info = useDebugInformation("ChildComponent", props);
+
+  return (
+    <>
+      <div>{props.boolean.toString()}</div>
+      <div>{props.count}</div>
+      <div>{JSON.stringify(info, null, 2)}</div>
+    </>
+  );
+}
+
+export default DebugInformationComponent;
