614beac07ba57c68121bfe6ba76ca4b6cb30201a
Andy Chen <chenyangpoppin@hotmail.com>
2023-07-23 22:02:46 +0800

    feat: implement the url switch logic

--- use/src/App.js
+++ use/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useId } from "react";
 import Data from "./Data";
 
 const URLS = {
@@ -9,10 +9,34 @@ const URLS = {
 
 function App() {
   const [url, setUrl] = useState(URLS.USERS);
+  const id = useId();
 
   return (
     <>
-      <div>USERS</div>
+      <div>
+        <input
+          type="radio"
+          id={`${id}-users`}
+          name="type"
+          defaultChecked
+          onClick={() => setUrl(URLS.USERS)}
+        />
+        <label htmlFor={`${id}-users`}>Users</label>
+        <input
+          type="radio"
+          id={`${id}-posts`}
+          name="type"
+          onClick={() => setUrl(URLS.POSTS)}
+        />
+        <label htmlFor={`${id}-posts`}>Posts</label>
+        <input
+          type="radio"
+          id={`${id}-comments`}
+          name="type"
+          onClick={() => setUrl(URLS.COMMENTS)}
+        />
+        <label htmlFor={`${id}-comments`}>Comments</label>
+      </div>
       <Data url={url} />
     </>
   );
